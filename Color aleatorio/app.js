15d7d05c0bb2229deb52b572925b3878b0b38980
lidiapons <you@example.com>
2024-03-03 13:29:08 +0100

    colorAleatorio

---
+++ Color aleatorio/app.js
@@ -0,0 +1,19 @@
+const boton = document.querySelector('.button');
+const color = document.querySelector('.color');
+
+function generarColorHexAleatorio() {
+    let digitos = '0123456789ABCDEF';
+    let colorHex = '#';
+
+    for (let i = 0; i < 6 ; i++) {
+        let indiceAleatorio = Math.floor(Math.random() * 16);
+        colorHex += digitos [indiceAleatorio];
+    }
+    return colorHex;
+}
+
+boton.addEventListener('click', function() {
+    let colorAleatorio = generarColorHexAleatorio();
+    color.textContent = colorAleatorio;
+    document.body.style.backgroundColor = colorAleatorio;
+});
